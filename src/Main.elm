a143c2f381053bb4d9a06d1ccaf3b0754174dafb
Henning Lund-Hanssen <henning.lund-hanssen@knowit.no>
2018-01-30 20:15:43 +0100

    Added WebSocket

--- src/Main.elm
+++ src/Main.elm
@@ -2,46 +2,77 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import WebSocket
 
 
 ---- MODEL ----
+
+
 type alias Model =
-    {}
+    { roomId : String }
+
+
+initialModel : Model
+initialModel =
+    { roomId = "" }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( initialModel, Cmd.none )
 
 
 
 ---- UPDATE ----
+
+
 type Msg
     = NoOp
-    | RoomIdChange
-    | RoomIdSubmit
+    | RoomIdOnChange String
+    | SubmitRoomId
+    | SubmitRoomIdResult String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        RoomIdOnChange roomId ->
+            ( { model | roomId = roomId }, Cmd.none )
+
+        SubmitRoomId ->
+            ( model, sendRoomId (getRoomId model) )
+
+        --- TODO: finish this!
+        SubmitRoomIdResult result ->
+            ( model, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    WebSocket.listen "ws://dummy.dumdum.org" SubmitRoomIdResult
 
 
 
 ---- VIEW ----
+
+
 view : Model -> Html Msg
 view model =
     div [ class "section" ]
         [ div [ class "container" ]
             [ div [ class "level" ]
                 [ div [ class "level-item" ]
-                    [ div [ class "field has-icons-left has-addons has-addons-centered"]
+                    [ div [ class "field has-icons-left has-addons has-addons-centered" ]
                         [ p [ class "control" ]
-                            [ button [ class "button is-warning is-static" ] [ text "#"] ]
+                            [ button [ class "button is-warning is-static" ] [ text "#" ] ]
                         , p [ class "control" ]
-                            [ input [ class "input", type_ "text", placeholder "Room ID" ] [] ]
+                            [ input [ class "input", type_ "text", placeholder "Room ID", onInput RoomIdOnChange ] [] ]
                         , p [ class "control" ]
-                            [ button [ class "button is-info" ] [ text "Join"] ]
+                            [ button [ class "button is-info", onClick SubmitRoomId ] [ text "Join" ] ]
                         ]
                     ]
                 ]
@@ -49,7 +80,10 @@ view model =
         ]
 
 
+
 ---- PROGRAM ----
+
+
 main : Program Never Model Msg
 main =
     Html.program
@@ -58,3 +92,13 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+getRoomId : Model -> String
+getRoomId model =
+    model.roomId
+
+
+sendRoomId : String -> Cmd msg
+sendRoomId roomId =
+    WebSocket.send "ws://dummy.dumdum.org" roomId
